fc093b96c6c65ba7c18b34bec5051d0df0e7407b
zhouguangming <zhouguangming1989@gmail.com>
2015-02-02 17:55:22 +0800

    refactor emoji parsing

--- lib/markdowner.rb
+++ lib/markdowner.rb
@@ -9,6 +9,7 @@ class Markdowner
     def markdowner
       @markdowner ||=
         Redcarpet::Markdown.new(renderer,
+         autolink: true,
          fenced_code_blocks: true,
          strikethrough: true,
          disable_indented_code_blocks: true,
@@ -17,22 +18,8 @@ class Markdowner
          table: true)
     end
 
-    def emojify(html)
-      html.gsub(/:([\w+-]+):/) do |match|
-        if emoji = Emoji.find_by_alias($1)
-          image_path = ActionController::Base.helpers.image_path("emoji/#{emoji.image_filename}")
-
-          %(<img alt='#$1' src='#{image_path}' style='vertical-align: middle' width='20' height='20' />)
-        else
-          match
-        end
-      end
-    end
-
     def render(body)
       html = markdowner.render(body)
-
-      emojify(html)
     end
   end
 
@@ -40,5 +27,25 @@ class Markdowner
     require 'rouge/plugins/redcarpet'
 
     include Rouge::Plugins::Redcarpet
+
+    def paragraph(text)
+      "<p>#{emojify(text)}</p>\n"
+    end
+
+    private
+
+    def emojify(text)
+      text.gsub(/:([\w+-]+):/) do |match|
+        if emoji = Emoji.find_by_alias($1)
+          image_path = ActionController::Base.helpers.image_path("emoji/#{emoji.image_filename}")
+          size       = "width='20' height='20'"
+          style      = 'vertical-align: middle'
+
+          %(<img alt='#$1' src='#{image_path}' style=#{style} #{size} />)
+        else
+          match
+        end
+      end
+    end
   end
 end
