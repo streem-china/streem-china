05609b46a97b688592d144bf34684b01304fae34
zhouguangming <zhouguangming1989@gmail.com>
2015-02-26 18:18:29 +0800

    remove useless code

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -7,4 +7,15 @@ class Favorite < ActiveRecord::Base
     uniqueness: { scope: [:favoritable_id, :favoritable_type] }
   validates :favoritable_id, presence: true
   validates :favoritable_type, presence: true
+
+  after_create :create_to_redis_after_create
+  after_destroy :destroy_from_redis_after_destroy
+
+  private
+
+  def store_to_redis_after_create
+  end
+
+  def destroy_from_redis_after_destroy
+  end
 end
