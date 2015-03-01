06912aa61db917af4fb4687052ebf8dcef0452b1
zhouguangming <zhouguangming1989@gmail.com>
2015-03-01 20:39:06 +0800

    adjust styleshett

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @user = User.find_by_name(params[:username])
 
     if @user
-      @topics = @user.topics.order('id desc').limit(20)
+      @topics = @user.topics.without_body.order('id desc').limit(20)
       @replies = @user.replies.order('id desc').limit(20)
       @favorites = @user.favorites.order('id desc').limit(20)
     else
