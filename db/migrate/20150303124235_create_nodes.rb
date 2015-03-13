a22dff4d88c6383f4b396b2d9565579cb60b4ce4
zhouguangming <zhouguangming1989@gmail.com>
2015-03-14 00:39:10 +0800

    change create node migration

--- db/migrate/20150303124235_create_nodes.rb
+++ db/migrate/20150303124235_create_nodes.rb
@@ -1,14 +1,8 @@
 class CreateNodes < ActiveRecord::Migration
-  def up
+  def change
     create_table :nodes do |t|
       t.string :name
       t.integer :topics_count, default: 0
     end
-
-    Node.create(id: 1, name: 'NoPoint')
-  end
-
-  def down
-    drop_table :nodes
   end
 end
