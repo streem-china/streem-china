0fb15f8ab131b7a013cecbb879effcc25d946fcb
zhouguangming <zhouguangming1989@gmail.com>
2015-03-17 17:59:29 +0800

    refactor

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -11,14 +11,15 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-    if params[:id]
-      @favorite = current_user.favorites.find(params[:id])
-    else
-      @favorite = current_user.favorites.where(
-        favoritable_id: params[:favoritable_id],
-        favoritable_type: params[:favoritable_type]
-      ).first
-    end
+    @favorite = if params[:id]
+                  current_user.favorites.find(params[:id])
+                else
+                  current_user.favorites.where(
+                    favoritable_id: params[:favoritable_id],
+                    favoritable_type: params[:favoritable_type]
+                  ).first
+                end
+
     if @favorite
       @favorite.destroy
 
