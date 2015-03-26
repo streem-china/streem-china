abd30795c1e6235c0dc78ec2a5b8775ffeb53454
zhouguangming <zhouguangming1989@gmail.com>
2015-03-26 12:09:24 +0800

    fix i18n

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,5 @@ module StreemChina
     # Do not swallow errors in after_commit/after_rollback callbacks.
 
     config.active_record.raise_in_transactional_callbacks = true
-
-    config.assets.initialize_on_precompile = true
   end
 end
