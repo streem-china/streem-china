1896a7b1b954895514e90d3d50a41aef587d9556
zhouguangming <zhouguangming1989@gmail.com>
2015-03-27 22:59:39 +0800

    adjust avatar margin

--- db/migrate/20150127132657_add_devise_to_users.rb
+++ db/migrate/20150127132657_add_devise_to_users.rb
@@ -3,7 +3,7 @@ class AddDeviseToUsers < ActiveRecord::Migration
     change_table(:users) do |t|
       ## Database authenticatable
       t.string :email, null: false, default: ""
-      #t.string :encrypted_password, null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
       #t.string   :reset_password_token
