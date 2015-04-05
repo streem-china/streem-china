5ba6fc4caa0bbf526c6d00958f39e878a8e1f885
zhouguangming <zhouguangming1989@gmail.com>
2015-04-05 18:44:33 +0800

    change to_s

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,7 +14,7 @@ module Users
       provider = auth.provider
       uid      = auth.uid
       name     = auth.info.nickname
-      email    = auth.info.email || ''
+      email    = auth.info.email.to_s
       avatar   = auth.info.image
 
       if authorization = Authorization.find_by_provider_and_uid(provider, uid)
