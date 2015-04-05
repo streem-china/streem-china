b52dc0f38d57848286d9229d8e238e6f2ed16bf9
zhouguangming <zhouguangming1989@gmail.com>
2015-04-05 20:43:03 +0800

    change db

--- db/schema.rb
+++ db/schema.rb
@@ -115,21 +115,21 @@ ActiveRecord::Schema.define(version: 20150313131334) do
   add_index "topics", ["user_id"], name: "index_topics_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                                    null: false
+    t.string   "name",                                   null: false
     t.string   "avatar"
-    t.integer  "topics_count",               default: 0,  null: false
-    t.integer  "replies_count",              default: 0,  null: false
-    t.integer  "favorites_count",            default: 0,  null: false
-    t.integer  "notifications_count",        default: 0,  null: false
-    t.integer  "unread_notifications_count", default: 0,  null: false
-    t.datetime "created_at",                              null: false
-    t.datetime "updated_at",                              null: false
-    t.string   "email",                      default: "", null: false
-    t.string   "encrypted_password",         default: "", null: false
+    t.integer  "topics_count",               default: 0, null: false
+    t.integer  "replies_count",              default: 0, null: false
+    t.integer  "favorites_count",            default: 0, null: false
+    t.integer  "notifications_count",        default: 0, null: false
+    t.integer  "unread_notifications_count", default: 0, null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "email"
+    t.string   "encrypted_password"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",              default: 0,  null: false
+    t.integer  "sign_in_count",              default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -138,7 +138,7 @@ ActiveRecord::Schema.define(version: 20150313131334) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",            default: 0,  null: false
+    t.integer  "failed_attempts",            default: 0, null: false
     t.string   "unlock_token"
     t.datetime "locked_at"
   end
