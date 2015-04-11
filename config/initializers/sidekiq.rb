d27811a21aa6880bcfd66611bb6321f9b70eb485
zhouguangming <zhouguangming1989@gmail.com>
2015-04-11 23:05:10 +0800

    fix sidekiq config var name

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
-config = { url: 'redis://localhost:6379/2', namespace: 'streem-china' }
+sidekiq_config = { url: 'redis://localhost:6379/2', namespace: 'streem-china' }
 
-Sidekiq.configure_server { |config| config.redis = config }
+Sidekiq.configure_server { |config| config.redis = sidekiq_config }
 
-Sidekiq.configure_client { |config| config.redis = config }
+Sidekiq.configure_client { |config| config.redis = sidekiq_config }
