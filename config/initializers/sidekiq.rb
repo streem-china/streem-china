b3cdc34fed05c9b4de8d288c544855737f4f8ea1
zhouguangming <zhouguangming1989@gmail.com>
2015-07-17 17:49:42 +0800

    change sidekiq redis namespace

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,4 @@
-sidekiq_config = { url: 'redis://localhost:6379/2', namespace: 'streem-china' }
+sidekiq_config = { url: 'redis://localhost:6379/2', namespace: 'streem-china:sidekiq' }
 
 Sidekiq.configure_server { |config| config.redis = sidekiq_config }
-
 Sidekiq.configure_client { |config| config.redis = sidekiq_config }
